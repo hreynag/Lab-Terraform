197f4d861c544abe92f9e20152d70982ea554232
Hector Alexander Reyna Gomez <hreynag@upao.edu.pe>
2025-04-13 18:21:44 -0500

    Creacioón de archivo Main/Principal

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_container" "container_server_web" {
+  name = "server_web"
+  image = "nginx:latest"
+  ports {
+    externa1 = 4000
+    internal = 80
+  }
+}
