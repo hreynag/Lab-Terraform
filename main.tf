6f18cbe751f577f6f7d5bab0911ba83c6b35cc73
Hector Reyna Gomez <hreynag@upao.edu.pe>
2025-04-13 19:27:13 -0500

    Contenedor Test BD MYSQL

--- main.tf
+++ main.tf
@@ -17,3 +17,23 @@ resource "docker_container" "container_server_web" {
     internal = 80
   }
 }
+
+# Contenedor para Conexión a BD MYSQL
+resource "docker_image" "mysql" {
+  name = "mysql:9.0.1"
+}
+
+resource "docker_container" "mysql_db" {
+  name  = "mysql-db"
+  image = docker_image.mysql.latest
+  env = [
+    "MYSQL_ROOT_PASSWORD=pass",
+    "MYSQL_DATABASE=Prueba_TestBD",
+    "MYSQL_USER=user_test",
+    "MYSQL_PASSWORD=test_1234"
+  ]
+  ports {
+    internal = 3306
+    external = 8084
+  }
+}
