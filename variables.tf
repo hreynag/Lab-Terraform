1ce163e822549d2bfd3246ec6b2a96cbce5aa21d
Hector Reyna Gomez <hreynag@upao.edu.pe>
2025-04-20 17:49:22 -0500

    Variables en 2 recursos

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "web_server_port" {
+  description = "Puerto de entrada del servidor web"
+  type = number
+  default = 4007
+}
+
+variable "server_db_port" {
+  description = "Puerto de entrada del servidor base de datos"
+  type = number
+  default = 3307
+}
